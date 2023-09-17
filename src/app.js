ccede85b6ecdd25254b9c550ca31468942e08bf4
TanishqSingla <tanishqsingla00@gmail.com>
2023-09-17 20:55:41 +0530

    fix: Project Restructure

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const routes = require("./routes");
+const { routeNotFound, errorHandler } = require("./middlewares");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/", routes);
+
+app.use(routeNotFound);
+app.use(errorHandler);

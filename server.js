e2ac0a7733eba6105e7da08c0c6ee48316e22277
Atifsid <atifsiddiqui35@gmail.com>
2023-10-08 19:48:57 +0530

    Add validation for env variables

--- server.js
+++ server.js
@@ -2,16 +2,35 @@ const dotenv = require("dotenv");
 
 const app = require("./app");
 
-if(process.env.NODE_ENV) {
-	dotenv.config({path: `.env.${process.env.NODE_ENV}`});
+const joi = require('joi');
+
+if (process.env.NODE_ENV) {
+	dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 } else {
 	dotenv.config();
+}
+
+const envSchema = joi
+	.object()
+	.keys({
+		NODE_ENV: joi
+			.string()
+			.valid("production", "development")
+			.required(),
+		PORT: joi.number().positive().required(),
+	})
+	.unknown();
+
+const { error, value } = envSchema.validate(process.env);
+
+if (error) {
+	throw new Error(`Config validation error: ${error.message}`);
 }
 
 const port = process.env.PORT;
 
 app.listen({ port }, () => {
-  console.log(`[Server] ready 🚀: http://localhost:${port}`);
+	console.log(`[Server] ready 🚀: http://localhost:${port}`);
 });
 
 module.exports = app;

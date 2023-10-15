77cf0603686cf692a5332528eaca75cb8bc961d6
aps08 <anoopprsingh.social@gmail.com>
2023-10-15 19:09:39 +0530

    accepthing both environment and file for firebase, no need passing env while run

--- server.js
+++ server.js
@@ -1,36 +1,23 @@
 const dotenv = require("dotenv");
-
-if (process.env.NODE_ENV !== "production") {
-	dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
-} else {
-	dotenv.config();
-}
-
 const app = require("./app");
 const joi = require("joi");
 
-
+dotenv.config();
 const envSchema = joi
 	.object()
 	.keys({
-		NODE_ENV: joi
-			.string()
-			.valid("production", "development")
-			.required(),
 		PORT: joi.number().positive().required(),
 	})
 	.unknown();
 
 const { error, value } = envSchema.validate(process.env);
-
 if (error) {
 	throw new Error(`Config validation error: ${error.message}`);
 }
 
 const port = process.env.PORT;
-
 app.listen({ port }, () => {
-	console.log(`[Server] ready 🚀: http://localhost:${port}`);
+	console.log(`[Server] running 🚀: http://localhost:${port}`);
 });
 
 module.exports = app;
--- utils/firestore.js
+++ utils/firestore.js
@@ -1,39 +1,34 @@
 const admin = require("firebase-admin");
+const fs = require("fs");
 
-if (!process.env.NODE_ENV || process.env.NODE_ENV === "production") {
-  const {
-    FIRESTORE_PROJECT_ID,
-    FIRESTORE_PRIVATE_KEY,
-    FIRESTORE_PRIVATE_ID,
-    FIRESTORE_CLIENT_EMAIL,
-    FIRESTORE_CLIENT_ID,
-    FIRESTORE_TOKEN_URI,
-    FIRESTORE_AUTH_PROVIDER,
-    FIRESTORE_CLIENT_CERT,
-    UNIVERSE_DOMAIN,
-    FIRESTORE_AUTH_URI,
-  } = process.env;
+const serviceAccountKey = "serviceAccountKey.json";
+let serviceAccountdetails;
 
-  admin.initializeApp({
-    credential: admin.credential.cert({
-        type: "service_account",
-        project_id: FIRESTORE_PROJECT_ID,
-        private_key_id: FIRESTORE_PRIVATE_ID,
-        private_key: FIRESTORE_PRIVATE_KEY,
-        client_email: FIRESTORE_CLIENT_EMAIL,
-        client_id: FIRESTORE_CLIENT_ID,
-        auth_uri: FIRESTORE_AUTH_URI,
-        token_uri: FIRESTORE_TOKEN_URI,
-        auth_provider_x509_cert_url: FIRESTORE_AUTH_PROVIDER,
-        client_x509_cert_url: FIRESTORE_CLIENT_CERT,
-        universe_domain: UNIVERSE_DOMAIN,
-      },
-    ),
-  });
-} else {
-	admin.initializeApp();
+if (fs.existsSync(serviceAccountKey)){
+  serviceAccountdetails = require("../" + serviceAccountKey);
+} else if (process.env.FIRESTORE_PROJECT_ID && process.env.FIRESTORE_PRIVATE_KEY && process.env.FIRESTORE_PRIVATE_ID && process.env.FIRESTORE_CLIENT_EMAIL && 
+  process.env.FIRESTORE_CLIENT_ID && process.env.FIRESTORE_TOKEN_URI && process.env.FIRESTORE_AUTH_PROVIDER && process.env.FIRESTORE_CLIENT_CERT &&
+  process.env.UNIVERSE_DOMAIN && process.env.FIRESTORE_AUTH_URI) {
+  serviceAccountdetails = {
+              type: "service_account",
+              project_id: process.env.FIRESTORE_PROJECT_ID,
+              private_key_id: process.env.FIRESTORE_PRIVATE_ID,
+              private_key: process.env.FIRESTORE_PRIVATE_KEY,
+              client_email: process.env.FIRESTORE_CLIENT_EMAIL,
+              client_id: process.env.FIRESTORE_CLIENT_ID,
+              auth_uri: process.env.FIRESTORE_AUTH_URI,
+              token_uri: process.env.FIRESTORE_TOKEN_URI,
+              auth_provider_x509_cert_url: process.env.FIRESTORE_AUTH_PROVIDER,
+              client_x509_cert_url: process.env.FIRESTORE_CLIENT_CERT,
+              universe_domain: process.env.UNIVERSE_DOMAIN,
+            };
+} else{
+  throw new Error("serviceAccountKey.json or environmental variables must be present for Firebase connection.");
 }
 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccountdetails)
+});
 const db = admin.firestore();
 
 module.exports = { db };

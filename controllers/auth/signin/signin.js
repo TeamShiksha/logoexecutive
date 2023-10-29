10447424de1c3ae28e66d4966d9bf930d09f9f78
TanishqSingla <tanishqsingla00@gmail.com>
2023-10-29 21:43:35 +0530

    fix: signin and singup payload

--- controllers/auth/signin/signin.js
+++ controllers/auth/signin/signin.js
@@ -1,20 +1,28 @@
 const Joi = require("joi");
 const { fetchUserByEmail } = require("../../../services/User");
 
-const loginPayloadSchema = Joi.object().keys({
-  email: Joi.string().required(),
-  password: Joi.string().required()
+const signinPayloadSchema = Joi.object().keys({
+  email: Joi.string()
+    .trim()
+    .required()
+    .regex(/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/)
+    .message("Email is not valid"),
+  password: Joi.string().trim().required().min(8).max(30),
 });
 
-async function loginController(req, res) {
+async function signinController(req, res) {
   try {
     const { body: payload } = req;
 
-    const { error, value } = loginPayloadSchema.validate(payload);
+    const { error, value } = signinPayloadSchema.validate(payload);
     if (!!error) {
       return res
         .status(422)
-        .json({ message: "Invalid payload", status: 422, error: error.message });
+        .json({
+          message: error.message,
+          statusCode: 422,
+          error: "unprocessable content"
+        });
     }
 
     const { email, password } = value;
@@ -40,13 +48,12 @@ async function loginController(req, res) {
     return res.status(200).json({
       user: user.data,
       token: user.generateJWT(),
-      expires_in: 24 * 60 * 60
+      expires_in: 24 * 60 * 60,
     });
-
   } catch (err) {
     console.log(err);
     throw err;
   }
 }
 
-module.exports = loginController;
+module.exports = signinController;

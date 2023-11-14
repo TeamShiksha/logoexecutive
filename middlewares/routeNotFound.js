43e607102a231af827971614895fe960b47d050c
TanishqSingla <tanishqsingla00@gmail.com>
2023-11-14 23:37:03 +0530

    fix: Updates and Fixes
    Add package-lock.json to gitignore to avoid files from npm install
    Fix signup payload and typo in error code
    Fix signup test filename typo
    Update routeNotFound response to match error message semantic
    Add firstName, lastName fields to User model
    Remove from signup jwt service

--- middlewares/routeNotFound.js
+++ middlewares/routeNotFound.js
@@ -1,5 +1,7 @@
 const routeNotFound = (_req, res, next) => {
-  res.status(404).json({ message: "route not found" });
+  res
+    .status(404)
+    .json({ statusCode: 404, message: "route not found", error: "not found" });
   next();
 };
 

63de7c48df34511b7fc39e961672c4be5239a61a
Pratiyush Kumar <pratiyush.inv@gmail.com>
2023-12-01 13:37:42 +0530

    exception handled and also edge cases handled

--- utils/cloudFront.js
+++ utils/cloudFront.js
@@ -1,15 +1,26 @@
 const cloudfrontSigner = require("@aws-sdk/cloudfront-signer");
 const getSignedUrl = cloudfrontSigner.getSignedUrl;
 
-const signedURLFunction = (distributionDomain, path) => {
-  //distributionDomain - Distribution domain name
-  //path - image name
-  return getSignedUrl({
-    url: `${distributionDomain}${path}`,
-    dateLessThan: new Date(Date.now() + 1000 * 60 * 60 * 24),
-    privateKey: process.env.CLOUD_FRONT_PRIVATE_KEY,
-    keyPairId: process.env.CLOUD_FRONT_KEYPAIR_ID,
-  });
+const signedURLFunction = (path) => {
+  try {
+    const distributionDomain = process.env.DISTRIBUTION_DOMAIN;
+    if (path !== "") {
+      return getSignedUrl({
+        url: `${distributionDomain}${path}`,
+        dateLessThan: new Date(Date.now() + 1000 * 60 * 5),
+        privateKey: process.env.CLOUD_FRONT_PRIVATE_KEY,
+        keyPairId: process.env.CLOUD_FRONT_KEYPAIR_ID,
+      });
+    } else {
+      return {
+        message: "image path is not defined",
+      };
+    }
+  } catch (err) {
+    return {
+      Error: err,
+    };
+  }
 };
 
 module.exports = { signedURLFunction };

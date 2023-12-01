53b0e5f39e58921c1d7cb0192808a9a973b99178
Pratiyush Kumar <pratiyush.inv@gmail.com>
2023-12-01 21:39:42 +0530

    added env variable and removed try catch block

--- utils/cloudFront.js
+++ utils/cloudFront.js
@@ -3,23 +3,17 @@ const cloudfrontSigner = require("@aws-sdk/cloudfront-signer");
 const getSignedUrl = cloudfrontSigner.getSignedUrl;
 
 function cloudFrontSignedURL(path) {
-  try {
-    const distributionDomain = process.env.DISTRIBUTION_DOMAIN;
-    if (path !== "") {
-      return getSignedUrl({
-        url: `${distributionDomain}${path}`,
-        dateLessThan: new Date(Date.now() + 1000 * 60 * 5),
-        privateKey: process.env.CLOUD_FRONT_PRIVATE_KEY,
-        keyPairId: process.env.CLOUD_FRONT_KEYPAIR_ID,
-      });
-    } else {
-      return {
-        message: "image path is not defined",
-      };
-    }
-  } catch (err) {
+  const distributionDomain = process.env.DISTRIBUTION_DOMAIN;
+  if (path !== "") {
+    return getSignedUrl({
+      url: `${distributionDomain}${path}`,
+      dateLessThan: new Date(Date.now() + 1000 * 60 * 5),
+      privateKey: process.env.CLOUD_FRONT_PRIVATE_KEY,
+      keyPairId: process.env.CLOUD_FRONT_KEYPAIR_ID,
+    });
+  } else {
     return {
-      Error: err,
+      message: "image path is not defined",
     };
   }
 }

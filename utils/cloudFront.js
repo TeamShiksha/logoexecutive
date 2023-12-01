fa017ee1d5368bba652f52d1cd4509c2d18541db
Pratiyush Kumar <pratiyush.inv@gmail.com>
2023-12-01 13:53:32 +0530

    signed url function created

---
+++ utils/cloudFront.js
@@ -0,0 +1,27 @@
+const cloudfrontSigner = require("@aws-sdk/cloudfront-signer");
+
+const getSignedUrl = cloudfrontSigner.getSignedUrl;
+
+function cloudFrontSignedURL(path) {
+  try {
+    const distributionDomain = process.env.DISTRIBUTION_DOMAIN;
+    if (path !== "") {
+      return getSignedUrl({
+        url: `${distributionDomain}${path}`,
+        dateLessThan: new Date(Date.now() + 1000 * 60 * 5),
+        privateKey: process.env.CLOUD_FRONT_PRIVATE_KEY,
+        keyPairId: process.env.CLOUD_FRONT_KEYPAIR_ID,
+      });
+    } else {
+      return {
+        message: "image path is not defined",
+      };
+    }
+  } catch (err) {
+    return {
+      Error: err,
+    };
+  }
+}
+
+module.exports = { cloudFrontSignedURL };

3f944f4b0f5d9b76f88a704a7f0135688e64b5ec
Pratiyush Kumar <pratiyush.inv@gmail.com>
2023-12-02 13:44:00 +0530

    added env schema validation

--- utils/cloudFront.js
+++ utils/cloudFront.js
@@ -5,15 +5,19 @@ const getSignedUrl = cloudfrontSigner.getSignedUrl;
 function cloudFrontSignedURL(path) {
   const distributionDomain = process.env.DISTRIBUTION_DOMAIN;
   if (path !== "") {
-    return getSignedUrl({
-      url: `${distributionDomain}${path}`,
-      dateLessThan: new Date(Date.now() + 1000 * 60 * 5),
-      privateKey: process.env.CLOUD_FRONT_PRIVATE_KEY,
-      keyPairId: process.env.CLOUD_FRONT_KEYPAIR_ID,
-    });
+    return {
+      data: getSignedUrl({
+        url: `${distributionDomain}${path}`,
+        dateLessThan: new Date(Date.now() + 1000 * 60 * 5),
+        privateKey: process.env.CLOUD_FRONT_PRIVATE_KEY,
+        keyPairId: process.env.CLOUD_FRONT_KEYPAIR_ID,
+      }),
+      success: true,
+    };
   } else {
     return {
       message: "image path is not defined",
+      success: false,
     };
   }
 }
--- utils/scripts/envSchema.test.js
+++ utils/scripts/envSchema.test.js
@@ -18,51 +18,83 @@ describe("Env Schema Validation", () => {
   describe("If service account key exists", () => {
     it("should return error message if port doesn't exists", () => {
       const env = {};
-      const result = validateEnv(env, {serviceAccountKey: true});
+      const result = validateEnv(env, { serviceAccountKey: true });
 
       expect(result).toHaveProperty("error");
       expect(result.error.message).toMatch(/\"PORT\" is required/gi);
     });
 
     it("should return error if port is not a valid number", () => {
-      const env = {PORT: "abc"};
-      const result = validateEnv(env, {serviceAccountKey: true});
+      const env = {
+        PORT: "abc",
+        CLOUD_FRONT_KEYPAIR_ID: "randomText",
+        CLOUD_FRONT_PRIVATE_KEY: "randomText",
+        DISTRIBUTION_DOMAIN: "randomText",
+      };
+      const result = validateEnv(env, { serviceAccountKey: true });
 
       expect(result.error).toBeTruthy();
       expect(result.error.message).toBe("PORT should be a number");
     });
 
     it("should return value if port exists", () => {
-      const env = {PORT: "3000"};
-      const result = validateEnv(env, {serviceAccountKey: true});
+      const env = {
+        PORT: "3000",
+        CLOUD_FRONT_KEYPAIR_ID: "randomText",
+        CLOUD_FRONT_PRIVATE_KEY: "randomText",
+        DISTRIBUTION_DOMAIN: "randomText",
+      };
+      const result = validateEnv(env, { serviceAccountKey: true });
 
       expect(result.error).toBeFalsy();
       expect(result).toHaveProperty("value");
-      expect(result.value).toStrictEqual({PORT: "3000"});
+      expect(result.value).toStrictEqual({
+        PORT: "3000",
+        CLOUD_FRONT_KEYPAIR_ID: "randomText",
+        CLOUD_FRONT_PRIVATE_KEY: "randomText",
+        DISTRIBUTION_DOMAIN: "randomText",
+      });
     });
 
     it("should be valid if port is a number", () => {
-      const env = {PORT: 3000};
-      const result = validateEnv(env, {serviceAccountKey: true});
+      const env = {
+        PORT: 3000,
+        CLOUD_FRONT_KEYPAIR_ID: "randomText",
+        CLOUD_FRONT_PRIVATE_KEY: "randomText",
+        DISTRIBUTION_DOMAIN: "randomText",
+      };
+      const result = validateEnv(env, { serviceAccountKey: true });
 
       expect(result.error).toBeFalsy();
       expect(result).toHaveProperty("value");
-      expect(result.value).toStrictEqual({PORT: 3000});
+      expect(result.value).toStrictEqual({
+        PORT: 3000,
+        CLOUD_FRONT_KEYPAIR_ID: "randomText",
+        CLOUD_FRONT_PRIVATE_KEY: "randomText",
+        DISTRIBUTION_DOMAIN: "randomText",
+      });
     });
   });
 
   describe("If service account key does not exists", () => {
     it("should validate with extended schema", () => {
-      const env = {PORT: "3000"};
+      const env = {
+        PORT: "3000",
+        CLOUD_FRONT_KEYPAIR_ID: "randomText",
+        CLOUD_FRONT_PRIVATE_KEY: "randomText",
+        DISTRIBUTION_DOMAIN: "randomText",
+      };
 
       const result = validateEnv(env);
 
       expect(result).toHaveProperty("error");
-      expect(result.error.message).toMatch(/\"FIRESTORE_PROJECT_ID\" is required/gi);
+      expect(result.error.message).toMatch(
+        /\"FIRESTORE_PROJECT_ID\" is required/gi
+      );
     });
 
     it("should return error if port does not exists", () => {
-      const env = {...validEnv};
+      const env = { ...validEnv };
       delete env.PORT;
 
       const result = validateEnv(env);

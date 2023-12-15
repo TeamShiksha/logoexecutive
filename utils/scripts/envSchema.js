5413ef684b6e7286b082f8816a9d2f1849a50e5f
Pratiyush Kumar <pratiyush.inv@gmail.com>
2023-12-15 13:58:50 +0530

    added test cases for the enviroment schema

--- utils/scripts/envSchema.js
+++ utils/scripts/envSchema.js
@@ -2,23 +2,32 @@ const Joi = require("joi");
 
 const EnvSchema = Joi.object()
   .keys({
-    PORT: Joi.alternatives(Joi.string().regex(/^\d+$/),
-      Joi.number()).required(),
+    PORT: Joi.alternatives(
+      Joi.string().regex(/^\d+$/),
+      Joi.number()
+    ).required(),
     BASE_URL: Joi.string().uri().required(),
     EMAIL_HOST: Joi.string().hostname().required(),
     EMAIL_SERVICE: Joi.string().required(),
-    EMAIL_PORT: Joi.alternatives(Joi.string().regex(/^\d+$/),
-      Joi.number()).required(),
+    EMAIL_PORT: Joi.alternatives(
+      Joi.string().regex(/^\d+$/),
+      Joi.number()
+    ).required(),
     EMAIL_SECURE: Joi.boolean().required(),
     EMAIL_USER: Joi.string().email().required(),
     EMAIL_PASS: Joi.string().required(),
-    CLOUD_FRONT_KEYPAIR_ID: Joi.string().regex(/^[A-Z0-9]+$/)
+    CLOUD_FRONT_KEYPAIR_ID: Joi.string()
+      .regex(/^[A-Z0-9]+$/)
       .required(),
     CLOUD_FRONT_PRIVATE_KEY: Joi.string().required(),
     DISTRIBUTION_DOMAIN: Joi.string()
       .uri({ scheme: ["https"] })
       .regex(/^https:\/\/[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/)
       .required(),
+    BUCKET_NAME: Joi.string().required(),
+    BUCKET_REGION: Joi.string().required(),
+    ACCESS_KEY: Joi.string().required(),
+    SECRET_ACCESS_KEY: Joi.string().required(),
   })
   .unknown(true);
 
--- utils/scripts/imageProcessing.js
+++ utils/scripts/imageProcessing.js
@@ -7,34 +7,53 @@ const buckeRegion = process.env.BUCKET_REGION;
 const accessKey = process.env.ACCESS_KEY;
 const secretAccesskey = process.env.SECRET_ACCESS_KEY;
 
-const imageProcessing = async (file) => {
+const imageProcessing = () => {
   try {
-    const isImageDataCreated = await createImageData(file);
-    if (isImageDataCreated) {
-      const s3 = new S3Client({
-        credentials: {
-          accessKeyId: accessKey,
-          secretAccessKey: secretAccesskey,
-        },
-        region: buckeRegion,
-      });
-      //imagePath value should be the path of image
-      //below is the temporary path which needs to be changed after cron implementation
-      const imagePath = `./images/${file}`;
-      const params = {
-        Bucket: bucketName,
-        Key: file,
-        Body: fs.readFileSync(imagePath),
-      };
-      const command = new PutObjectCommand(params);
-      await s3.send(command);
-      return { message: "Data Inserted in db", statusCode: 201, error: "OK" };
-    }
-    return {
-      message: "Data already present in db",
-      statusCode: 409,
-      error: "Conflict",
-    };
+    //imagePath value should be the path of image
+    //below is the temporary path which needs to be changed after cron implementation
+    const imagePath = "./images";
+    fs.readdir(imagePath, (err, files) => {
+      if (err) {
+        return {
+          message: "something went wrong!",
+          statusCode: 401,
+          error: err,
+        };
+      } else {
+        files.forEach(async (image) => {
+          const isImageDataCreated = await createImageData(image);
+          if (isImageDataCreated) {
+            const s3 = new S3Client({
+              credentials: {
+                accessKeyId: accessKey,
+                secretAccessKey: secretAccesskey,
+              },
+              region: buckeRegion,
+            });
+            //imagePath value should be the path of image
+            //below is the temporary path which needs to be changed after cron implementation
+            const imagePath = `./images/${image}`;
+            const params = {
+              Bucket: bucketName,
+              Key: file,
+              Body: fs.readFileSync(imagePath),
+            };
+            const command = new PutObjectCommand(params);
+            await s3.send(command);
+            return {
+              message: "Data Inserted in db",
+              statusCode: 201,
+              error: "OK",
+            };
+          }
+          return {
+            message: "Data already present in db",
+            statusCode: 409,
+            error: "Conflict",
+          };
+        });
+      }
+    });
   } catch (error) {
     return {
       message: error,

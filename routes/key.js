055c0d60c625a8993329f12a82de931eddc5368e
Anoop Pratap Singh <anoopprsingh.social@gmail.com>
2023-12-21 22:35:58 +0530

    Update key.js

--- routes/key.js
+++ routes/key.js
@@ -4,6 +4,6 @@ const authMiddleware = require("../middlewares/auth");
 const {deleteKeyController} = require("../controllers/keys");
 
 router.post("/generate", authMiddleware, generateKey);
-router.delete("/delete", authMiddleware, deleteKeyController);
+router.delete("/destroy", authMiddleware, deleteKeyController);
 
 module.exports = router;

9e950d7b345d4e42449fc4c966b7bca93164896e
Anoop Pratap Singh <anoopprsingh.social@gmail.com>
2024-01-03 23:59:37 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,6 @@ const authRouter = require("./auth");
 const keyRouter = require("./key");
 const updateProfileRouter = require("./updateProfile");
 
-router.get("/", (_req, res) =>
-  res.status(200).json({ message: "Welcome to Logo executive API" })
-);
-
 router.use("/auth", authRouter);
 router.use("/users", userRouter);
 router.use("/contact", contactUsRouter);

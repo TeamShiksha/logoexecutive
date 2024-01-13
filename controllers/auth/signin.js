e96a1db427720f848d31b0d1735783d79be42432
TanishqSingla <tanishqsingla00@gmail.com>
2024-01-13 18:14:36 +0530

    feat: update signin api

--- controllers/auth/signin.js
+++ controllers/auth/signin.js
@@ -1,8 +1,9 @@
 const Joi = require("joi");
 const { fetchUserByEmail } = require("../../services/User");
 const { fetchSubscriptionByuserid } = require("../../services/Subscription");
-const { fetchKeyByuserid } = require("../../services/Key");
-const jwt = require("jsonwebtoken");
+const { fetchKeysByuserid } = require("../../services/Key");
+const { STATUS_CODES } = require("http");
+const dayjs = require("dayjs");
 
 const signinPayloadSchema = Joi.object().keys({
   email: Joi.string()
@@ -13,67 +14,69 @@ const signinPayloadSchema = Joi.object().keys({
   password: Joi.string().trim().required().min(8).max(30),
 });
 
-async function signinController(req, res) {
+async function signinController(req, res, next) {
   try {
     const { body: payload } = req;
     const { error, value } = signinPayloadSchema.validate(payload);
-    if (!!error) {
+    if (!!error)
       return res.status(422).json({
         message: error.message,
         statusCode: 422,
-        error: "unprocessable content",
+        error: STATUS_CODES[422],
       });
-    }
 
     const { email, password } = value;
     const user = await fetchUserByEmail(email);
-    if (!user) {
-      return res.status(400).json({
-        error: "bad request",
-        message: "Email or Password incorrect",
-        statusCode: 400,
-      });
-    }
-    if(user.isUserVerified()) {
+    if (!user)
       return res.status(401).json({
-        error: "unauthorized access",
+        error: STATUS_CODES[401],
+        message: "Email or Password incorrect",
+        statusCode: 401,
+      });
+
+    if(!user.isVerified)
+      return res.status(401).json({
+        error: STATUS_CODES[401],
         message: "Email is not verified",
         statusCode: 401
       });
-    }
 
     const matchPassword = await user.matchPassword(password);
     if (!matchPassword) {
-      return res.status(400).json({
-        error: "bad request",
+      return res.status(401).json({
+        error: STATUS_CODES[401],
         message: "Email or Password incorrect",
-        statusCode: 400,
+        statusCode: 401,
       });
     }
-    const subscription = await fetchSubscriptionByuserid(user.userId);
-    const keys = await fetchKeyByuserid(user.userId);
 
-    res.cookie("jwt", user.generateJWT());
-    return res.status(200).json({
+    const [subscription, keys] = await Promise.all([fetchSubscriptionByuserid(user.userId), fetchKeysByuserid(user.userId)]);
+
+    const subscriptionData = subscription && {
+      subscriptionType: subscription.subscriptionType,
+      usageLimit: subscription.usageLimit,
+      isActive: subscription.isActive,
+      keyLimit: subscription.keyLimit
+    };
+
+    if(!subscription)
+      res.status(206);
+    else
+      res.status(200);
+
+    res.cookie("jwt", user.generateJWT(), { expires: dayjs().add(1, "day").toDate() });
+    return res.json({
       data: {
         email: user.data.email,
         firstName: user.data.firstName,
         lastName: user.data.lastName,
-        subcription: {
-          subscriptionType: subscription.subscriptionType,
-          usageLimit: subscription.usageLimit,
-          isActive: subscription.isActive,
-          keyLimit: subscription.keyLimit
-        },
-        keys: {
-          ...keys
-        }
+        subscription: subscriptionData,
+        keys: keys || null,
       },
       message: "Succesfully signed in",
     });
   } catch (err) {
-    console.log(err);
-    throw err;
+    next(err);
   }
 }
 

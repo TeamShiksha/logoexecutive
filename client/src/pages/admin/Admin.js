9edfa78b223c767f1e96b908b67d02f925155010
Vinayak <myselfvinayakhegde@gmail.com>
2024-02-17 17:15:50 +0530

    feat: add titles for admin page sections

--- client/src/pages/admin/Admin.js
+++ client/src/pages/admin/Admin.js
@@ -3,6 +3,7 @@ import DragAndDrop from '../../components/admindashboard/DragAndDrop';
 import ImageTable from '../../components/admindashboard/ImageTable';
 import {dummyUploadedImageDetails} from '../../constants';
 import './Admin.css';
+import Divider from '../../components/common/divider/Divider';
 
 const AdminDashboard = () => {
 	const [uploadedImages, setUploadedImages] = useState(
@@ -10,8 +11,15 @@ const AdminDashboard = () => {
 	);
 	return (
 		<div className='admin-container'>
-			<DragAndDrop setUploadedImages={setUploadedImages} />
-			<ImageTable uploadedImages={uploadedImages} />
+			<section className='admin-section-container'>
+				<h2>Add Image</h2>
+				<DragAndDrop setUploadedImages={setUploadedImages} />
+				<ImageTable uploadedImages={uploadedImages} />
+			</section>
+			<Divider />
+			<section className='admin-section-container'>
+				<h2>Add Admin</h2>
+			</section>
 		</div>
 	);
 };

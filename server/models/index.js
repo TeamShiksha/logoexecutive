04b3088fc3cb6a506a978b9063da99dcac618850
aps08 <anoopprsingh.social@gmail.com>
2024-02-18 16:53:34 +0530

    refactor/model server model test case passing

---
+++ server/models/index.js
@@ -0,0 +1,10 @@
+import ContactUs from "./ContactUs";
+import Keys from "./Keys";
+import Users from "./Users";
+import UserToken from "./UserToken";
+import Subscriptions from "./Subscriptions";
+import Images from "./Images";
+
+module.exports = {
+  ContactUs, Keys, Users, UserToken, Subscriptions, Images
+};

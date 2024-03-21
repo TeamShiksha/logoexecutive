6cfb44b434960574851034af6aa265b938a64dde
Vinayak Hegde <myselfvinayakhegde@gmail.com>
2024-03-21 22:24:28 +0530

    Correct Frontend tests (#184)
    * tests: fix header tests
    
    * fix: signincard tests
    
    * test: increase header coverage
    
    * remove unused import
    
    * revert api instance
    
    * Update api_instance.js
    
    ---------
    
    Co-authored-by: Anoop Pratap Singh <anoopprsingh.social@gmail.com>

--- client/src/components/header/header.test.js
+++ client/src/components/header/header.test.js
@@ -1,32 +1,41 @@
-import {render, screen} from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import Header from './Header';
 import * as router from 'react-router';
 import {BrowserRouter} from 'react-router-dom';
+import {AuthContext} from '../../contexts/AuthContext';
 
 describe('Header', () => {
-	const mockLoggedOutUser = jest.fn();
 	const navigate = jest.fn();
+	const mockLogout = jest.fn();
+
+	const renderHeader = (isAuthenticated) => {
+		render(
+			<AuthContext.Provider
+				value={{
+					isAuthenticated,
+					logout: mockLogout,
+				}}
+			>
+				<BrowserRouter>
+					<Header />
+				</BrowserRouter>
+			</AuthContext.Provider>,
+		);
+	};
 
 	beforeEach(() => {
 		jest.spyOn(router, 'useNavigate').mockImplementation(() => navigate);
 	});
+
 	test('should show logo', () => {
-		render(
-			<BrowserRouter>
-				<Header user='test' logoutUser={mockLoggedOutUser} />
-			</BrowserRouter>,
-		);
+		renderHeader(false);
 		const image = screen.getByAltText('brand logo');
 		expect(image).toBeInTheDocument();
 		expect(screen.getByText('LogoExecutive')).toBeInTheDocument();
 	});
 
-	test('should show different navigation links when user is logged in', () => {
-		render(
-			<BrowserRouter>
-				<Header user='test' logoutUser={mockLoggedOutUser} />
-			</BrowserRouter>,
-		);
+	test('should show Dashboard, Docs, Pricing, About, and Logout links when user is logged in', () => {
+		renderHeader(true);
 		expect(screen.getByText('Dashboard')).toBeInTheDocument();
 		expect(screen.getByText('Docs')).toBeInTheDocument();
 		expect(screen.getByText('Pricing')).toBeInTheDocument();
@@ -34,16 +43,59 @@ describe('Header', () => {
 		expect(screen.getByText('Logout')).toBeInTheDocument();
 	});
 
-	test('should show different navigation links when user is logged out', () => {
-		render(
-			<BrowserRouter>
-				<Header user='' logoutUser={mockLoggedOutUser} />
-			</BrowserRouter>,
-		);
+	test('should show Home, Demo, Pricing, About, and Get Started links when user is logged out', () => {
+		renderHeader(false);
 		expect(screen.getByText('Home')).toBeInTheDocument();
 		expect(screen.getByText('Demo')).toBeInTheDocument();
 		expect(screen.getByText('Pricing')).toBeInTheDocument();
 		expect(screen.getByText('About')).toBeInTheDocument();
 		expect(screen.getByText('Get Started')).toBeInTheDocument();
+	});
+
+	test('should call handleLogout and navigate to /welcome when Logout button is clicked', () => {
+		renderHeader(true);
+		const logoutButton = screen.getByText('Logout');
+		fireEvent.click(logoutButton);
+		expect(mockLogout).toHaveBeenCalled();
+		expect(navigate).toHaveBeenCalledWith('/welcome');
+	});
+
+	test('should navigate to /signin when Get Started button is clicked', () => {
+		renderHeader(false);
+		const getStartedButton = screen.getByText('Get Started');
+		fireEvent.click(getStartedButton);
+		expect(navigate).toHaveBeenCalledWith('/signin');
+	});
+
+	test('should toggle navigation bar visibility when burger menu is clicked', () => {
+		renderHeader(false);
+		global.window.innerWidth = 800;
+		fireEvent.resize(global.window);
+		const burgerMenu = screen.getByTestId('burger-menu');
+		fireEvent.click(burgerMenu);
+		expect(screen.getByRole('navigation')).toBeInTheDocument();
+		fireEvent.click(burgerMenu);
+		expect(screen.queryByRole('navigation')).toBeNull();
+	});
+
+	test('should change header background on scroll', () => {
+		renderHeader(false);
+		expect(screen.getByRole('banner')).not.toHaveClass('bg');
+		global.window.scrollY = 100;
+		fireEvent.scroll(global.window);
+		expect(screen.getByRole('banner')).toHaveClass('bg');
+		global.window.scrollY = 0;
+		fireEvent.scroll(global.window);
+		expect(screen.getByRole('banner')).not.toHaveClass('bg');
+	});
+
+	test('should hide navigation bar when window width is less than 1001', () => {
+		renderHeader(false);
+		global.window.innerWidth = 800;
+		fireEvent.resize(global.window);
+		expect(screen.queryByRole('navigation')).toBeNull();
+		global.window.innerWidth = 1200;
+		fireEvent.resize(global.window);
+		expect(screen.getByRole('navigation')).toBeInTheDocument();
 	});
 });

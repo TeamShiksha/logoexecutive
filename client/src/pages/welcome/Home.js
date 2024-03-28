96a3fd99e1be2e6f8af876b5b45f026180133297
krishna saini <krishnapraanjal10@gmail.com>
2024-03-28 22:10:42 +0530

    fix: home page redirection for logged in user

--- client/src/pages/welcome/Home.js
+++ client/src/pages/welcome/Home.js
@@ -1,9 +1,17 @@
+import {useContext} from 'react';
+import {Navigate} from 'react-router-dom';
 import Demo from '../../components/demo/Demo';
-import HeroSection from '../../components/herosection/HeroSection';
+import {HeroSection} from '../../components/herosection/HeroSection';
 import FAQs from '../../components/faqs/FAQs';
+import {AuthContext} from '../../contexts/AuthContext';
 import './Home.css';
 
-function Home() {
+const Home = () => {
+	const {isAuthenticated} = useContext(AuthContext);
+
+	if (isAuthenticated) {
+		return <Navigate to='/dashboard' />;
+	}
 	return (
 		<div className='home-container'>
 			<HeroSection />
@@ -11,6 +19,6 @@ function Home() {
 			<FAQs />
 		</div>
 	);
-}
+};
 
 export default Home;

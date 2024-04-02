97c517cbf218086ffe9bee93f977fb1397428fe9
Pratiyush Kumar <82165483+Pratiyushkumar@users.noreply.github.com>
2024-04-02 21:21:25 +0530

    [BUG] - Form Validation handling | Forgot Password  (#223)
    * form validation handling
    
    * changed the position of error message
    
    * forgot password css updated
    
    * Update ForgotPassword.js
    
    ---------
    
    Co-authored-by: Anoop Pratap Singh <anoopprsingh.social@gmail.com>

--- client/src/pages/forgot-password/ForgotPassword.js
+++ client/src/pages/forgot-password/ForgotPassword.js
@@ -7,6 +7,7 @@ import './ForgotPassword.css';
 function ForgotPassword() {
 	const [userEmail, setUserEmail] = useState('');
 	const [isSuccess, setIsSuccess] = useState(false);
+	const [validationError, setValidationError] = useState(null);
 	const {
 		data: response,
 		makeRequest,
@@ -21,14 +22,29 @@ function ForgotPassword() {
 	function handleUserEmailChange(e) {
 		setUserEmail(e.target.value);
 	}
+	const validateFormData = () => {
+		if (!userEmail) {
+			return 'Please enter your email address';
+		} else if (!/\S+@\S+\.\S+/.test(userEmail)) {
+			return 'Please enter a valid email address';
+		} else {
+			return null;
+		}
+	};
+
 	async function handleSubmit(e) {
 		e.preventDefault();
 		setIsSuccess(false);
-		const success = await makeRequest();
-
-		if (success) {
-			setIsSuccess(true);
-			setUserEmail('');
+		setValidationError(null);
+		const error = validateFormData();
+		if (error) {
+			setValidationError(error);
+		} else {
+			const success = await makeRequest();
+			if (success) {
+				setIsSuccess(true);
+				setUserEmail('');
+			}
 		}
 	}
 
@@ -39,15 +55,9 @@ function ForgotPassword() {
 				<p className='forgot-password-instruction'>
 					Enter the email you used to create your account.
 				</p>
-				{errorMsg && (
-					<p
-						className='forgot-password-error'
-						aria-live='assertive'
-						role='alert'
-					>
-						{errorMsg}
-					</p>
-				)}
+				<p className='forgot-password-error' aria-live='assertive' role='alert'>
+					{errorMsg || validationError || ''}
+				</p>
 				{isSuccess && (
 					<p
 						className='forgot-password-success'
@@ -59,7 +69,6 @@ function ForgotPassword() {
 				)}
 				<form onSubmit={handleSubmit}>
 					<CustomInput
-						type='email'
 						label='Enter Your Email'
 						value={userEmail}
 						name='userEmail'

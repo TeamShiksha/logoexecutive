a49d301c2b720a6050bc440fc2d22f921aa88f2b
Anoop Pratap Singh <anoopprsingh.social@gmail.com>
2024-04-09 22:27:53 +0530

    Update ResetPassword.test.js

--- client/src/pages/reset-password/ResetPassword.test.js
+++ client/src/pages/reset-password/ResetPassword.test.js
@@ -10,12 +10,10 @@ describe('ResetPassword component', () => {
 				<ResetPassword />
 			</BrowserRouter>,
 		);
-
 		expect(screen.getByText('Reset Password')).toBeInTheDocument();
 		expect(
 			screen.getByText('Enter your new password below'),
 		).toBeInTheDocument();
-
 		expect(screen.getByLabelText('New Password')).toBeInTheDocument();
 		expect(screen.getByLabelText('Confirm Password')).toBeInTheDocument();
 		expect(screen.getByRole('button', {name: 'Submit'})).toBeInTheDocument();
@@ -27,16 +25,13 @@ describe('ResetPassword component', () => {
 				<ResetPassword />
 			</BrowserRouter>,
 		);
-
 		fireEvent.change(screen.getByLabelText('New Password'), {
 			target: {value: 'password123'},
 		});
 		fireEvent.change(screen.getByLabelText('Confirm Password'), {
 			target: {value: 'password456'},
 		});
-
-		fireEvent.click(screen.getByRole('button', {name: 'Submit'}));
-
+		fireEvent.click(screen.getByRole('button', {name: 'Submit'}))
 		await waitFor(() => {
 			expect(
 				screen.getByText(
@@ -52,14 +47,12 @@ describe('ResetPassword component', () => {
 				<ResetPassword />
 			</BrowserRouter>,
 		);
-
 		fireEvent.change(screen.getByLabelText('New Password'), {
 			target: {value: 'password123'},
 		});
 		fireEvent.change(screen.getByLabelText('Confirm Password'), {
 			target: {value: 'password123'},
 		});
-
 		fireEvent.click(screen.getByRole('button', {name: 'Submit'}));
 		await waitFor(() => {
 			expect(screen.getByText('Password Reset Successful')).toBeInTheDocument();

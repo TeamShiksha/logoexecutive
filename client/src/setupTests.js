beba105de2bb19c28d1bc8e434d51955991032ff
Vinayak Hegde <myselfvinayakhegde@gmail.com>
2024-04-10 15:27:00 +0530

    Integrate MSW (#230)
    * refactor: improve signin form validation
    
    * feat: integrate msw
    
    * remove dummy user file
    
    * fix signin test
    
    * modify signincard tests to use msw
    
    * refactor: correct signin handler file name

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,1 +1,11 @@
 import '@testing-library/jest-dom';
+import {server} from './mocks/server';
+beforeAll(() => {
+	server.listen();
+});
+afterEach(() => {
+	server.resetHandlers();
+});
+afterAll(() => {
+	server.close();
+});

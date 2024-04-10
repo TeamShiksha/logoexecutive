f4ec9cfd619982898c1ece1937815abb922a659c
Anoop Pratap Singh <anoopprsingh.social@gmail.com>
2024-04-10 14:43:42 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,8 @@ import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import {AuthProvider} from './contexts/AuthContext';
-import './index.css';
 import {UserProvider} from './contexts/UserContext';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 

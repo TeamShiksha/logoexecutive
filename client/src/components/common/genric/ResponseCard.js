0098afd4df662610e5c642b54dbfcf389e142810
shubhamp <shubhamp@ataloud.com>
2024-04-11 14:31:34 +0530

    Refactor : Changed the reset classnames to response

--- client/src/components/common/genric/ResponseCard.js
+++ client/src/components/common/genric/ResponseCard.js
@@ -1,4 +1,3 @@
-import {IconContext} from 'react-icons/lib';
 import {FaCheck} from 'react-icons/fa6';
 import {RxCross2} from 'react-icons/rx';
 import PropTypes from 'prop-types';
@@ -6,19 +5,22 @@ import './ResponseCard.css';
 
 function ResponseCard({countdown, successMsg, errorMsg, title}) {
 	return (
-		<section className='reset-success-card-wrapper' role='reset-success-alert'>
+		<section
+			className='response-success-card-wrapper'
+			role='response-success-alert'
+		>
 			{errorMsg ? (
-				<div className='reset-failure-card'>
-					<RxCross2 className='reset-failure-icon' />
-					<h3 className='reset-success-title'>{title}</h3>
-					<p className='reset-success-message'>{errorMsg}</p>
+				<div className='response-failure-card'>
+					<RxCross2 className='response-failure-icon' />
+					<h3 className='response-success-title'>{title}</h3>
+					<p className='response-success-message'>{errorMsg}</p>
 				</div>
 			) : (
-				<div className='reset-success-card'>
-					<FaCheck className='reset-success-icon' />
-					<h3 className='reset-success-title'>{title}</h3>
-					<p className='reset-success-message'>{successMsg}</p>
-					<div className='reset-success-countdown'>
+				<div className='response-success-card'>
+					<FaCheck className='response-success-icon' />
+					<h3 className='response-success-title'>{title}</h3>
+					<p className='response-success-message'>{successMsg}</p>
+					<div className='response-success-countdown'>
 						<p>Redirecting to sign in </p>
 						<p>{countdown}</p>
 					</div>

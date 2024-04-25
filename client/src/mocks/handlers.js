1a2fa7ea69800f4da99c93a269f972bb4fe05a66
Joy <56365512+ardourApeX@users.noreply.github.com>
2024-04-25 22:02:34 +0530

    Updated tests and created custom hook (#266)
    * WIP : Fixing test for email verification using msw
    
    * Test : Updated test with msw
    
    * Feat : Added custom redirect hook
    
    * Feat : Utilised custom redirect hook
    
    * Chore : Updated yarn lock
    
    * Chore : Updated test file name
    
    * Chore : Removed unused dependency
    
    * Chore : Moved test file for a hook

--- client/src/mocks/handlers.js
+++ client/src/mocks/handlers.js
@@ -1,6 +1,11 @@
 import {signinHandler} from './handlers/signin-handler';
 import {signupHandler} from './handlers/signup-handler';
+import {emailVerificationHandler} from './handlers/emailVerification-handler';
 
-const handlers = [...signinHandler, ...signupHandler];
+const handlers = [
+	...signinHandler,
+	...signupHandler,
+	...emailVerificationHandler,
+];
 
 export default handlers;

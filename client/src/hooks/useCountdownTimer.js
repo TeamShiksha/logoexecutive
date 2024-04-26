c1b4a4e46cbcad420e137a9fce69ad8df862d4f6
aps08 <anoopprsingh.social@gmail.com>
2024-04-26 12:14:32 +0530

    used the usecounter hook and working fine

---
+++ client/src/hooks/useCountdownTimer.js
@@ -0,0 +1,30 @@
+import {useEffect} from 'react';
+
+const useCountdownTimer = (
+	isSuccess,
+	navigate,
+	countdown,
+	setCountdown,
+	route = '/welcome',
+) => {
+	useEffect(() => {
+		let timer = null;
+		if (isSuccess) {
+			timer = setInterval(() => {
+				if (countdown > 1) {
+					setCountdown((prevCount) => prevCount - 1);
+				} else {
+					clearInterval(timer);
+					navigate(route);
+				}
+			}, 1000);
+		}
+		return () => {
+			clearInterval(timer);
+		};
+	}, [isSuccess, countdown, navigate]);
+
+	return countdown;
+};
+
+export default useCountdownTimer;

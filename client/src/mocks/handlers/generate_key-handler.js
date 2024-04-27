ee7d9421597787b44e6145cefc2014279506cdaa
Aasish <46561984+Aasishm@users.noreply.github.com>
2024-04-27 18:29:38 +0530

    Update generate_key-handler.js

--- client/src/mocks/handlers/generate_key-handler.js
+++ client/src/mocks/handlers/generate_key-handler.js
@@ -5,25 +5,25 @@ export const generateKeyHandler = [
 		return res(
 			ctx.status(200),
 			ctx.json({
-                "message": "Key generated successfully",
-                "statusCode": 200,
-                "data": {
-                    "keyId": "d0cdc208-ecb6-4161-9c00-a5f5db66cfab",
-                    "keyDescription": "Test API Key",
-                    "key": "7D8ED2F3F2C748BC9B96CED7EE2DE1BF",
-                    "usageCount": 0,
-                    "createdAt": new Date().toLocaleDateString('en-US', {
-                        day: '2-digit',
-                        month: 'short',
-                        year: 'numeric',
-                    }),
-                    "updatedAt": new Date().toLocaleDateString('en-US', {
-                        day: '2-digit',
-                        month: 'short',
-                        year: 'numeric',
-                    })
-                }
-            }),
+				message: 'Key generated successfully',
+				statusCode: 200,
+				data: {
+					keyId: 'd0cdc208-ecb6-4161-9c00-a5f5db66cfab',
+					keyDescription: 'Test API Key',
+					key: '7D8ED2F3F2C748BC9B96CED7EE2DE1BF',
+					usageCount: 0,
+					createdAt: new Date().toLocaleDateString('en-US', {
+						day: '2-digit',
+						month: 'short',
+						year: 'numeric',
+					}),
+					updatedAt: new Date().toLocaleDateString('en-US', {
+						day: '2-digit',
+						month: 'short',
+						year: 'numeric',
+					}),
+				},
+			}),
 		);
 	}),
 ];

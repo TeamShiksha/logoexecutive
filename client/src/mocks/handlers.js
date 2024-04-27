54d952723c8ff48881fce7f554379e6c9cc040ee
Aasish <46561984+Aasishm@users.noreply.github.com>
2024-04-27 18:27:23 +0530

    Update handlers.js

--- client/src/mocks/handlers.js
+++ client/src/mocks/handlers.js
@@ -1,4 +1,4 @@
-import { generateKeyHandler } from './handlers/generate_key-handler';
+import {generateKeyHandler} from './handlers/generate_key-handler';
 import {signinHandler} from './handlers/signin-handler';
 import {signupHandler} from './handlers/signup-handler';
 import {emailVerificationHandler} from './handlers/emailVerification-handler';

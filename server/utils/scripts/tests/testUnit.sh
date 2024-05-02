17a92855b646dba0f347a9992412bb094529b99b
aps08 <anoopprsingh.social@gmail.com>
2024-05-02 23:01:09 +0530

    resolve remulator issues and remove the sh use to avoid issues

--- server/utils/scripts/tests/testUnit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# set 'test' environment
-export NODE_ENV='test'
-
-# get project_id value from firestore config
-
-echo 'Start firestore emulator and run unit tests:'
-firebase emulators:exec 'yarn test' --project=logoexecutive-d1e9a

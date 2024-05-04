c767d73010f8111617bdd33c3df5b8edff13eadb
Pratiyush Kumar <pratiyush.inv@gmail.com>
2024-05-04 22:35:59 +0530

    Api Doc testing done

--- client/src/pages/docs/ApiDocs.js
+++ client/src/pages/docs/ApiDocs.js
@@ -2,6 +2,7 @@ import {useEffect, useState} from 'react';
 import ReactMarkdown from 'react-markdown';
 import gfm from 'remark-gfm';
 import './ApiDocs.css';
+import Spinner from '../../components/spinner/Spinner';
 
 const components = {
 	code({className, children, ...props}) {
@@ -16,26 +17,39 @@ const components = {
 function ApiDocs() {
 	const file_name = 'docs.md';
 	const [post, setPost] = useState('');
+	const [isLoading, setIsLoading] = useState(false);
+	const [showErrMssg, setShowErrMssg] = useState('');
+
 	useEffect(() => {
 		const fetchMarkdown = async () => {
+			setIsLoading(true);
 			try {
 				const res = await import(`../../../../docs/${file_name}`);
 				const response = await fetch(res.default);
 				const text = await response.text();
+				setIsLoading(false);
 				setPost(text);
 			} catch (err) {
-				console.log(err);
+				setIsLoading(false);
+				setShowErrMssg('File Not Found!');
 			}
 		};
 		fetchMarkdown();
 	}, []);
 
 	return (
-		<section className='markdown-body'>
-			<ReactMarkdown components={components} remarkPlugins={[gfm]}>
-				{post}
-			</ReactMarkdown>
-		</section>
+		<>
+			<section className='markdown-body'>
+				{post ? (
+					<ReactMarkdown components={components} remarkPlugins={[gfm]}>
+						{post}
+					</ReactMarkdown>
+				) : (
+					<div className='errorMessage'>{showErrMssg}</div>
+				)}
+			</section>
+			{isLoading && <Spinner />}
+		</>
 	);
 }
 

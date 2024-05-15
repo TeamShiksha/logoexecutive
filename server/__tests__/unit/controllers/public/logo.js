31b61e04d78fff59e3181afd46153895515ef14d
Aasish M <maasish@fortinet.com>
2024-05-15 12:14:55 +0530

    Public logo API for demo

--- server/__tests__/unit/controllers/public/logo.js
+++ server/__tests__/unit/controllers/public/logo.js
@@ -1,17 +1,14 @@
 const request = require("supertest");
 const { STATUS_CODES } = require("http");
 const app = require("../../../../app");
-const { KeyService, ImageService } = require("../../../../services");
+const { ImageService } = require("../../../../services");
 
-jest.mock("../../../../services/Keys", () => ({
-  isAPIKeyPresent: jest.fn()
-}));
 jest.mock("../../../../services/Images", () => ({
   fetchImageByCompanyFree: jest.fn()
 }));
 
 const mockCDNLink = "https://du4goljobz66l.cloudfront.net/meta.png?Expires=1706882374&Key-Pair-Id=K1CBTPCVEWK03E&Signature=l6ZpbQ-Z3WtJQ8inaDomAAAhcnnC0U2R~5Su7HWjC8fbbeQI4e4JBK368guQFYtc8rQAJMur446ozoXJE-9Hcj125NlZFSMqpeUsjam-nk9Wb2d8XGR6UjyxYLqGbhca8WYwl~h0CzHbe20PJXZbyuFPTufCrBTkIoh4o3Mg3MQDe2fPf5z6L9xLgVtbOrpJQoHZ0YlWTNvWJWutL-AFX8KbisrBaMi8zRa6h-mSfXuIoUyjziMRA5gPA0T8QSUJ8iLdbURwWxvRpRpM0Ohrjk06sWDSTkNzLL~pVNyL7LwO04mAHVK4XYgK5179xcZ-BjMMW1qJD3YF7G~xdcsXJw__";
-const ENDPOINT = "/api/business/logo";
+const ENDPOINT = "/api/public/logo";
 
 describe("getLogoController", () => {
 
@@ -25,22 +22,8 @@ describe("getLogoController", () => {
     jest.resetAllMocks();
   });
 
-  it("422 - API key is required", async () => {
-    const mockQuery = {"domain": "coupang"};
-    const response = await request(app)
-      .get(ENDPOINT)
-      .query(mockQuery);
-
-    expect(response.status).toBe(422);
-    expect(response.body).toEqual({
-      message: "API key is required",
-      statusCode: 422,
-      error: STATUS_CODES[422]
-    });
-  });
-
   it("422 - Domain is required", async () => {
-    const mockQuery = {"API_KEY": "2B1B1BF5F9914BCD85A0B1122C71EDDB"};
+    const mockQuery = {};
     const response = await request(app)
       .get(ENDPOINT)
       .query(mockQuery);
@@ -53,23 +36,22 @@ describe("getLogoController", () => {
     });
   });
 
-  it("403 - Invalid API key", async () => {
-    const mockQuery = {"domain": "coupang", "API_KEY": "2B1B1BF5F9914BCD85A0B1122C71EDDC"};
+  it("422 - Domain should not be empty", async () => {
+    const mockQuery = {"domain": ""};
     const response = await request(app)
       .get(ENDPOINT)
       .query(mockQuery);
 
-    expect(response.status).toBe(403);
+    expect(response.status).toBe(422);
     expect(response.body).toEqual({
-      message: "Invalid API key",
-      statusCode: 403,
-      error: STATUS_CODES[403]
+      message: "\"domain\" is not allowed to be empty",
+      statusCode: 422,
+      error: STATUS_CODES[422]
     });
   });
 
   it("404 - Logo not available", async () => {
-    jest.spyOn(KeyService, "isAPIKeyPresent").mockImplementation(() => true);
-    const mockQuery = {"domain": "infibeam", "API_KEY": "2B1B1BF5F9914BCD85A0B1122C71EDDB"};
+    const mockQuery = {"domain": "infibeam"};
     const response = await request(app)
       .get(ENDPOINT)
       .query(mockQuery);
@@ -83,8 +65,8 @@ describe("getLogoController", () => {
   });
 
   it("500 - Unexpected error", async () => {
-    jest.spyOn(KeyService, "isAPIKeyPresent").mockImplementation(() => {throw new Error("Unexpected error");});
-    const mockQuery = {"domain": "coupang", "API_KEY": "2B1B1BF5F9914BCD85A0B1122C71EDDB"};
+    jest.spyOn(ImageService, "fetchImageByCompanyFree").mockImplementation(() => {throw new Error("Unexpected error");});
+    const mockQuery = {"domain": "coupang"};
     const response = await request(app)
       .get(ENDPOINT)
       .query(mockQuery);
@@ -98,9 +80,8 @@ describe("getLogoController", () => {
   });
 
   it("200 - CDN url created for company name", async () => {
-    jest.spyOn(KeyService, "isAPIKeyPresent").mockImplementation(() => true);
     jest.spyOn(ImageService, "fetchImageByCompanyFree").mockImplementation(() => mockCDNLink);
-    const mockQuery = {"domain": "coupang", "API_KEY": "2B1B1BF5F9914BCD85A0B1122C71EDDB"};
+    const mockQuery = {"domain": "coupang"};
     const response = await request(app)
       .get(ENDPOINT)
       .query(mockQuery);
@@ -113,9 +94,8 @@ describe("getLogoController", () => {
   });
 
   it("Success response when domain is given", async () => {
-    jest.spyOn(KeyService, "isAPIKeyPresent").mockImplementation(() => true);
     jest.spyOn(ImageService, "fetchImageByCompanyFree").mockImplementation(() => mockCDNLink);
-    const mockQuery = {"domain": "www.coupang.com", "API_KEY": "2B1B1BF5F9914BCD85A0B1122C71EDDB"};
+    const mockQuery = {"domain": "www.coupang.com"};
     const response = await request(app)
       .get(ENDPOINT)
       .query(mockQuery);
@@ -128,9 +108,8 @@ describe("getLogoController", () => {
   });
 
   it("Success when company url is given", async () => {
-    jest.spyOn(KeyService, "isAPIKeyPresent").mockImplementation(() => true);
     jest.spyOn(ImageService, "fetchImageByCompanyFree").mockImplementation(() => mockCDNLink);
-    const mockQuery = {"domain": "https://www.coupang.com", "API_KEY": "2B1B1BF5F9914BCD85A0B1122C71EDDB"};
+    const mockQuery = {"domain": "https://www.coupang.com"};
     const response = await request(app)
       .get(ENDPOINT)
       .query(mockQuery);
--- server/routes/public.js
+++ server/routes/public.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const contactUsController = require("../controllers/public/contact-us");
+const demoLogoController = require("../controllers/public/logo");
 
 router.post("/contact-us", contactUsController);
+router.get("/logo", demoLogoController);
 
 module.exports = router; 

bf2b813c02b90cebf1d6499bca5f59e40aadcf79
Vinayak <myselfvinayakhegde@gmail.com>
2024-05-28 15:34:36 +0530

    feat: add adminOnly option for ProtectedRoute

--- client/src/utils/ProtectedRoute.js
+++ client/src/utils/ProtectedRoute.js
@@ -1,11 +1,33 @@
 import PropTypes from 'prop-types';
-import {useContext} from 'react';
+import {useContext, useEffect} from 'react';
 import {AuthContext} from '../contexts/AuthContext';
 import {Navigate, useLocation} from 'react-router';
+import {UserContext} from '../contexts/UserContext';
+import Spinner from '../components/spinner/Spinner';
 
-function ProtectedRoute({children}) {
+function ProtectedRoute({adminOnly, children}) {
 	const {isAuthenticated} = useContext(AuthContext);
+	const {userData, loading, fetchUserData} = useContext(UserContext);
 	const location = useLocation();
+
+	useEffect(() => {
+		if (adminOnly && isAuthenticated && (loading || !userData)) {
+			fetchUserData();
+		}
+	}, []);
+
+	if (adminOnly && isAuthenticated && (loading || !userData)) {
+		return <Spinner />;
+	}
+
+	if (adminOnly && isAuthenticated) {
+		if (userData.userType === 'ADMIN') {
+			return children;
+		} else {
+			return <Navigate to='/welcome' replace={true} />;
+		}
+	}
+
 	return isAuthenticated ? (
 		children
 	) : (
@@ -15,6 +37,7 @@ function ProtectedRoute({children}) {
 
 ProtectedRoute.propTypes = {
 	children: PropTypes.node.isRequired,
+	adminOnly: PropTypes.bool,
 };
 
 export default ProtectedRoute;

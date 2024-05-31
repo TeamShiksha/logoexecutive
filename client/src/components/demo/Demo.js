323456e2e0afcf399c75c2d24892096ca3c2edfd
sagarb2003 <sagarssb2003@gmail.com>
2024-05-31 12:13:55 +0530

    added go button for fetching the logo and updated the test for Demo.js

--- client/src/components/demo/Demo.js
+++ client/src/components/demo/Demo.js
@@ -1,32 +1,50 @@
 import {useState} from 'react';
 import CustomInput from '../common/input/CustomInput';
 import './Demo.css';
+import {useApi} from '../../hooks/useApi';
+import Spinner from '../spinner/Spinner';
 
 const Demo = () => {
 	const [brandName, setBrandName] = useState('');
+	const {errorMsg, makeRequest, data, loading} = useApi({
+		url: `api/public/logo`,
+		method: 'get',
+		params: {domain: brandName},
+	});
 	const handleBrandNameChange = (event) => {
 		setBrandName(event.target.value);
 	};
-
+	const handleFormChange = async (e) => {
+		e.preventDefault();
+		await makeRequest();
+	};
 	return (
 		<section id='demo' className='demo-container'>
-			<h2 className='demo-heading'>Try it now</h2>
-			<p
-				className='demo-input-description'
-				data-testid='demo-input-description'
-			>
-				Enter the name of a brand or the URL of a website for which you would
-				like to retrieve logos.
-			</p>
-			<CustomInput
-				type='text'
-				label='Brand name'
-				value={brandName}
-				name='brandname'
-				onChange={handleBrandNameChange}
-			/>
+			<form onSubmit={handleFormChange} noValidate data-testid='demo-form'>
+				<h2 className='demo-heading'>Try it now</h2>
+				<p
+					className='demo-input-description'
+					data-testid='demo-input-description'
+				>
+					Enter the name of a brand or the URL of a website for which you would
+					like to retrieve logos.
+				</p>
+				<CustomInput
+					type='text'
+					label='Brand name'
+					value={brandName}
+					name='brandname'
+					onChange={handleBrandNameChange}
+				/>
+				<button className='demo-button' type='submit' disabled={loading}>
+					{loading ? <Spinner /> : 'Go'}
+				</button>
+			</form>
+			{errorMsg && <p className='image-display-error'>{errorMsg}</p>}
+			<div className='demo-displaylogo'>
+				{!errorMsg && data && brandName && <img src={data.data} alt='Logo' />}
+			</div>
 		</section>
 	);
 };
-
 export default Demo;
--- client/src/components/demo/Demo.test.js
+++ client/src/components/demo/Demo.test.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import {fireEvent, render, screen} from '@testing-library/react';
+import {fireEvent, render, screen, waitFor} from '@testing-library/react';
 import Demo from './Demo';
+import {useApi} from '../../hooks/useApi';
+
+jest.mock('../../hooks/useApi');
 
 describe('Demo Component', () => {
+	beforeEach(() => {
+		useApi.mockReturnValue({
+			errorMsg: '',
+			makeRequest: jest.fn(),
+			data: null,
+			loading: false,
+		});
+	});
 	test('renders a text input field', () => {
 		render(<Demo />);
 		expect(screen.getByText('Try it now')).toBeInTheDocument();
@@ -22,5 +33,56 @@ describe('Demo Component', () => {
 		fireEvent.change(brandNameInput, {target: {value: 'google'}});
 		fireEvent.change(brandNameInput, {target: {value: ''}});
 		expect(brandNameInput.value).toBe('');
+	});
+	test('form submission triggers makeRequest', async () => {
+		const makeRequestMock = jest.fn();
+		useApi.mockReturnValue({
+			errorMsg: '',
+			makeRequest: makeRequestMock,
+			data: null,
+			loading: false,
+		});
+		render(<Demo />);
+		const brandNameInput = screen.getByLabelText('Brand name');
+		fireEvent.change(brandNameInput, {target: {value: 'google'}});
+		const form = screen.getByTestId('demo-form');
+		fireEvent.submit(form);
+
+		await waitFor(() => {
+			expect(makeRequestMock).toHaveBeenCalled();
+		});
+	});
+	test('displays spinner while data is being fetched', async () => {
+		useApi.mockReturnValue({
+			errorMsg: '',
+			makeRequest: jest.fn(
+				() => new Promise((resolve) => setTimeout(resolve, 1000)),
+			),
+			data: null,
+			loading: true,
+		});
+		render(<Demo />);
+	});
+	test('displays error message when errorMsg is present', () => {
+		useApi.mockReturnValue({
+			errorMsg: 'Error fetching data',
+			makeRequest: jest.fn(),
+			data: null,
+			loading: false,
+		});
+		render(<Demo />);
+		expect(screen.getByText('Error fetching data')).toBeInTheDocument();
+	});
+	test('displays logo image when data is fetched', () => {
+		useApi.mockReturnValue({
+			errorMsg: '',
+			makeRequest: jest.fn(),
+			data: {data: 'logo-url'},
+			loading: false,
+		});
+		render(<Demo />);
+		const brandNameInput = screen.getByLabelText('Brand name');
+		fireEvent.change(brandNameInput, {target: {value: 'google'}});
+		expect(screen.getByAltText('Logo').src).toContain('logo-url');
 	});
 });

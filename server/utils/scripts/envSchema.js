d7b05c30a60edd843d0a7656f2c59e0128c8c5f7
Vinayak <myselfvinayakhegde@gmail.com>
2024-06-01 00:42:31 +0530

    reduce envs

--- server/utils/scripts/envSchema.js
+++ server/utils/scripts/envSchema.js
@@ -2,15 +2,9 @@ const Joi = require("joi");
 
 const EnvSchema = Joi.object()
   .keys({
+    ACCESS_KEY: Joi.string().required(),
     BASE_URL: Joi.string().uri().required(),
-    EMAIL_HOST: Joi.string().hostname().required(),
-    EMAIL_SERVICE: Joi.string().required(),
-    EMAIL_PORT: Joi.alternatives(
-      Joi.string().regex(/^\d+$/),
-      Joi.number()
-    ).required(),
-    EMAIL_USER: Joi.string().email().required(),
-    EMAIL_PASS: Joi.string().required(),
+    BUCKET_NAME: Joi.string().required(),
     CLOUD_FRONT_KEYPAIR_ID: Joi.string()
       .regex(/^[A-Z0-9]+$/)
       .required(),
@@ -19,24 +13,33 @@ const EnvSchema = Joi.object()
       .uri({ scheme: ["https"] })
       .regex(/^https:\/\/[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/)
       .required(),
-    BUCKET_NAME: Joi.string().required(),
-    BUCKET_REGION: Joi.string().required(),
-    ACCESS_KEY: Joi.string().required(),
+    EMAIL_PASS: Joi.string().required(),
+    EMAIL_USER: Joi.string().email().required(),
+    FIREBASE_ADMINSDK_ID: Joi.string().required(),
+    FIRESTORE_CLIENT_ID: Joi.string().required(),
+    FIRESTORE_PRIVATE_ID: Joi.string().required(),
+    FIRESTORE_PRIVATE_KEY: Joi.string().required(),
+    FIRESTORE_PROJECT_ID: Joi.string().required(),
+    JWT_SECRET: Joi.string().required(),
     SECRET_ACCESS_KEY: Joi.string().required(),
   })
   .unknown(true);
 
 const EnvSchemaExtended = EnvSchema.append({
-  FIRESTORE_PROJECT_ID: Joi.string().required(),
-  FIRESTORE_PRIVATE_KEY: Joi.string().required(),
-  FIRESTORE_PRIVATE_ID: Joi.string().required(),
-  FIRESTORE_CLIENT_EMAIL: Joi.string().required(),
-  FIRESTORE_CLIENT_ID: Joi.string().required(),
-  FIRESTORE_TOKEN_URI: Joi.string().required(),
+  BUCKET_REGION: Joi.string().required(),
+  EMAIL_HOST: Joi.string().hostname().required(),
+  EMAIL_PORT: Joi.alternatives(
+    Joi.string().regex(/^\d+$/),
+    Joi.number()
+  ).required(),
+  EMAIL_SERVICE: Joi.string().required(),
   FIRESTORE_AUTH_PROVIDER: Joi.string().required(),
-  FIRESTORE_CLIENT_CERT: Joi.string().required(),
-  UNIVERSE_DOMAIN: Joi.string().required(),
   FIRESTORE_AUTH_URI: Joi.string().required(),
+  FIRESTORE_CLIENT_CERT: Joi.string().required(),
+  FIRESTORE_CLIENT_EMAIL: Joi.string().required(),
+  FIRESTORE_TOKEN_URI: Joi.string().required(),
+  KEY: Joi.string().required(),
+  UNIVERSE_DOMAIN: Joi.string().required(),
 }).unknown(true);
 
 /**
@@ -45,10 +48,7 @@ const EnvSchemaExtended = EnvSchema.append({
  * @param {Object} [flags={}] - flags for validation
  * @param {boolean} flags.serviceAccountKey - Validate schema when service account key file exists
  **/
-const validateEnv = (env, flags = {}) => {
-  if (flags.serviceAccountKey) {
-    return EnvSchema.validate(env);
-  }
+const validateEnv = (env) => {
   return EnvSchemaExtended.validate(env);
 };
 

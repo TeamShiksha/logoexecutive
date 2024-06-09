34aaaac6156269dcdb502b8928b3027412ccb617
amankumarsingh77 <amankumarsingh7702@gmal.com>
2024-06-09 09:52:58 +0530

    Shift /function/api.js to app.js

--- server/app.js
+++ server/app.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+const { validateEnv } = require("./utils/scripts/envSchema.js");
+
+dotenv.config();
+const { error } = validateEnv(process.env);
+if (error) {
+  console.log(`Config validation error: ${error.message}`);
+  process.exit(1);
+}
 
 const routes = require("./routes");
 const { routeNotFound, errorHandler } = require("./middlewares");
@@ -8,10 +17,10 @@ const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
-
 app.use("/api/", routes);
-
 app.use(routeNotFound);
 app.use(errorHandler);
+
+app.listen(process.env.PORT,()=>{console.log(`Server listening at port ${process.env.PORT}`);});
 
 module.exports = app;

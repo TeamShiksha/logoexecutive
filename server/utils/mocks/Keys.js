389f0b894c9112adc4b79888468869bfecadf19f
DeltaDynamo <159301654+DeltaDynamo@users.noreply.github.com>
2024-07-16 23:34:45 +0530

    Migrate Keys Services & its test from firebase to mongodb

--- server/utils/mocks/Keys.js
+++ server/utils/mocks/Keys.js
@@ -1,14 +1,13 @@
-const { Timestamp } = require("firebase-admin/firestore");
+const mongoose = require("mongoose");
 
 const mockKeys = [
   {
-    userId: "0c1266ab-8ad2-4ab9-b56c-e1db6982f120",
-    keyId: "e132bdde-e0f4-4f62-88dd-06b06c5f5af0",
-    key: "e4f8df44-3761-4b7d-b204-c01471521c27",
+    user: new mongoose.Types.ObjectId("DCFFDBC27D679F85E0289E78"),
+    key: "15C557FB641A43A96E5A3C19",
     keyDescription: "API-KEY-1",
     usageCount: 200,
-    createdAt: Timestamp.fromDate(new Date("01-01-2001")),
-    updatedAt: Timestamp.fromDate(new Date("01-01-2001")),
+    createdAt: Date.now(),
+    updatedAt: Date.now(),
   },
 ];
 

07a6670c6253ade4975333a51c5edf1437cca034
anandbaraik <anandbaraik2014@gmail.com>
2024-07-21 13:58:56 +0530

    feat:show confirmation modal on click of delete api key button :white_check_mark:

--- client/src/components/dashboard/ApiKeyTable.js
+++ client/src/components/dashboard/ApiKeyTable.js
@@ -3,8 +3,24 @@ import {FiCopy} from 'react-icons/fi';
 import {LuCopyCheck} from 'react-icons/lu';
 import {MdDeleteOutline} from 'react-icons/md';
 import {formatDate} from '../../utils/helpers';
-
+import Modal from '../common/modal/Modal';
+import { useRef, useState } from 'react';
 function ApiKeyTable({keys, copiedKey, handleCopyToClipboard, deleteKey}) {
+
+	const apiKeyToBeDeleted = useRef(null);
+	const [modalOpen, setModalOpen] = useState(false);
+
+	const showConfirmationModal = (keyId) => {
+		apiKeyToBeDeleted.current = keyId;
+		setModalOpen(true);
+	}
+
+	const confirmActionHandler = () => {
+		setModalOpen(false);
+		deleteKey(apiKeyToBeDeleted.current);
+		apiKeyToBeDeleted.current = null;
+	}
+
 	return (
 		<section className='dashboard-content-section'>
 			<div className='dashboard-content-item api-key-table'>
@@ -51,7 +67,7 @@ function ApiKeyTable({keys, copiedKey, handleCopyToClipboard, deleteKey}) {
 									<button
 										className='api-key-delete-button'
 										data-testid='api-key-delete'
-										onClick={() => deleteKey(key.keyId)}
+										onClick={() => showConfirmationModal(key.keyId)}
 									>
 										<MdDeleteOutline />
 									</button>
@@ -62,6 +78,17 @@ function ApiKeyTable({keys, copiedKey, handleCopyToClipboard, deleteKey}) {
 					</tbody>
 				</table>
 			</div>
+			<Modal
+				modalOpen={modalOpen}
+				setModal={setModalOpen}
+				showButtons={true}
+				handleConfirm={confirmActionHandler}
+			>
+				<h2>Are you sure?</h2>
+				<p className='settings-confirm-message'>
+					Please confirm that you want to delete this permanently, as this action cannot be undone.
+				</p>
+			</Modal>
 		</section>
 	);
 }

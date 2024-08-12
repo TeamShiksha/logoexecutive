b2f67154bb1b5ac401e9489f978e5ab178bd770e
sharathxct <sharathxct@gmail.com>
2024-08-13 01:06:26 +0530

    Removed visibility toggle in API Key Copy

--- client/src/components/dashboard/ApiKey.js
+++ client/src/components/dashboard/ApiKey.js
@@ -1,51 +1,42 @@
-import {IoMdClose} from 'react-icons/io';
+import { IoMdClose } from 'react-icons/io';
 import PropTypes from 'prop-types';
-import {FaCopy, FaEye, FaEyeSlash} from 'react-icons/fa';
-import {useState} from 'react';
+import { FaCopy, } from 'react-icons/fa';
 
-function ApiKey({Key, handleCloseKey, handleCopyToClipboard}) {
-	const [hideKey, setHideKey] = useState(true);
-	return (
-		<div className='dashboard-content-item'>
-			<div className='current-plan-header'>
-				<p className='api-key-warning'>
-					Make sure to copy your API KEY now. You won’t be able to see it again!
-				</p>
+function ApiKey({ Key, handleCloseKey, handleCopyToClipboard }) {
+  return (
+    <div className='dashboard-content-item'>
+      <div className='current-plan-header'>
+        <p className='api-key-warning'>
+          Make sure to copy your API KEY now. You won’t be able to see it again!
+        </p>
 
-				<button
-					className='api-key-buttons'
-					onClick={() => handleCloseKey()}
-					aria-label='Close'
-				>
-					<IoMdClose />
-				</button>
-			</div>
-			<div className='api-key-div dashboard-content-item'>
-				<p>{hideKey ? '*******************' : Key}</p>
-				<div className='api-key-buttons-div'>
-					<button
-						className='api-key-buttons'
-						onClick={() => setHideKey(!hideKey)}
-						aria-label='Show/Hide API Key'
-					>
-						{hideKey ? <FaEye /> : <FaEyeSlash />}{' '}
-					</button>
-					<button
-						className='api-key-buttons'
-						onClick={() => handleCopyToClipboard(Key)}
-						aria-label='Copy API Key'
-					>
-						<FaCopy />
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+        <button
+          className='api-key-buttons'
+          onClick={() => handleCloseKey()}
+          aria-label='Close'
+        >
+          <IoMdClose />
+        </button>
+      </div>
+      <div className='api-key-div dashboard-content-item'>
+        <p>*******************</p>
+        <div className='api-key-buttons-div'>
+          <button
+            className='api-key-buttons'
+            onClick={() => handleCopyToClipboard(Key)}
+            aria-label='Copy API Key'
+          >
+            <FaCopy />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 ApiKey.propTypes = {
-	Key: PropTypes.string.isRequired,
-	handleCloseKey: PropTypes.func.isRequired,
-	handleCopyToClipboard: PropTypes.func.isRequired,
+  Key: PropTypes.string.isRequired,
+  handleCloseKey: PropTypes.func.isRequired,
+  handleCopyToClipboard: PropTypes.func.isRequired,
 };
 
 export default ApiKey;

ac31d128cc1b5bff6fea9a3630e813624637eca7
sharathxct <sharathxct@gmail.com>
2024-08-13 01:11:33 +0530

    Prettier

--- client/src/components/dashboard/ApiKey.test.js
+++ client/src/components/dashboard/ApiKey.test.js
@@ -1,63 +1,62 @@
-import { render, fireEvent, screen } from '@testing-library/react';
+import {render, fireEvent, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import ApiKey from './ApiKey';
 
 describe('ApiKey', () => {
-  it('renders correctly', () => {
-    const handleCloseKey = jest.fn();
-    const handleCopyToClipboard = jest.fn();
-    const key = 'test_api_key';
+	it('renders correctly', () => {
+		const handleCloseKey = jest.fn();
+		const handleCopyToClipboard = jest.fn();
+		const key = 'test_api_key';
 
-    render(
-      <ApiKey
-        Key={key}
-        handleCloseKey={handleCloseKey}
-        handleCopyToClipboard={handleCopyToClipboard}
-      />,
-    );
+		render(
+			<ApiKey
+				Key={key}
+				handleCloseKey={handleCloseKey}
+				handleCopyToClipboard={handleCopyToClipboard}
+			/>,
+		);
 
-    expect(
-      screen.getByText(
-        'Make sure to copy your API KEY now. You won’t be able to see it again!',
-      ),
-    ).toBeInTheDocument();
-    expect(screen.getByText('*******************')).toBeInTheDocument();
-  });
+		expect(
+			screen.getByText(
+				'Make sure to copy your API KEY now. You won’t be able to see it again!',
+			),
+		).toBeInTheDocument();
+		expect(screen.getByText('*******************')).toBeInTheDocument();
+	});
 
-  it('closes when the close button is clicked', () => {
-    const handleCloseKey = jest.fn();
-    const handleCopyToClipboard = jest.fn();
-    const key = 'test_api_key';
+	it('closes when the close button is clicked', () => {
+		const handleCloseKey = jest.fn();
+		const handleCopyToClipboard = jest.fn();
+		const key = 'test_api_key';
 
-    render(
-      <ApiKey
-        Key={key}
-        handleCloseKey={handleCloseKey}
-        handleCopyToClipboard={handleCopyToClipboard}
-      />,
-    );
+		render(
+			<ApiKey
+				Key={key}
+				handleCloseKey={handleCloseKey}
+				handleCopyToClipboard={handleCopyToClipboard}
+			/>,
+		);
 
-    const closeButton = screen.getByRole('button', { name: 'Close' });
-    fireEvent.click(closeButton);
-    expect(handleCloseKey).toHaveBeenCalledTimes(1);
-  });
+		const closeButton = screen.getByRole('button', {name: 'Close'});
+		fireEvent.click(closeButton);
+		expect(handleCloseKey).toHaveBeenCalledTimes(1);
+	});
 
-  it('copies the API key to clipboard when clicked', () => {
-    const handleCloseKey = jest.fn();
-    const handleCopyToClipboard = jest.fn();
-    const key = 'test_api_key';
+	it('copies the API key to clipboard when clicked', () => {
+		const handleCloseKey = jest.fn();
+		const handleCopyToClipboard = jest.fn();
+		const key = 'test_api_key';
 
-    render(
-      <ApiKey
-        Key={key}
-        handleCloseKey={handleCloseKey}
-        handleCopyToClipboard={handleCopyToClipboard}
-      />,
-    );
+		render(
+			<ApiKey
+				Key={key}
+				handleCloseKey={handleCloseKey}
+				handleCopyToClipboard={handleCopyToClipboard}
+			/>,
+		);
 
-    const copyButton = screen.getByLabelText('Copy API Key');
-    userEvent.click(copyButton);
-    expect(handleCopyToClipboard).toHaveBeenCalledWith(key);
-  });
-
+		const copyButton = screen.getByLabelText('Copy API Key');
+		userEvent.click(copyButton);
+		expect(handleCopyToClipboard).toHaveBeenCalledWith(key);
+	});
 });

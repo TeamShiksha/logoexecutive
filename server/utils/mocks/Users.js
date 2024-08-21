dd84e9688af58ad9c3209c0124a9b9437754e6bf
Abhishek <asharma991@outlook.com>
2024-08-22 02:23:51 +0530

    feat: add new mock for operator

--- server/utils/mocks/Users.js
+++ server/utils/mocks/Users.js
@@ -4,7 +4,7 @@ const { default: mongoose } = require("mongoose");
 
 const mockUsers = [
   {
-    _id : new mongoose.Types.ObjectId(),
+    _id: new mongoose.Types.ObjectId(),
     firstName: "John",
     lastName: "Doe",
     email: "john.doe@example.com",
@@ -12,10 +12,10 @@ const mockUsers = [
     createdAt: Date.now(),
     updatedAt: Date.now(),
     userType: UserType.CUSTOMER,
-    isVerified: false,
+    isVerified: false
   },
   {
-    _id : new mongoose.Types.ObjectId(),
+    _id: new mongoose.Types.ObjectId(),
     firstName: "John",
     lastName: "Doe",
     email: "johndoe@example.com",
@@ -23,10 +23,10 @@ const mockUsers = [
     createdAt: Date.now(),
     updatedAt: Date.now(),
     userType: UserType.CUSTOMER,
-    isVerified: true,
+    isVerified: true
   },
   {
-    _id : new mongoose.Types.ObjectId(),
+    _id: new mongoose.Types.ObjectId(),
     firstName: "John",
     lastName: "Doe",
     email: "johndoe2@example.com",
@@ -34,8 +34,19 @@ const mockUsers = [
     createdAt: Date.now(),
     updatedAt: Date.now(),
     userType: UserType.ADMIN,
-    isVerified: true,
+    isVerified: true
   },
+  {
+    _id: new mongoose.Types.ObjectId(),
+    firstName: "John",
+    lastName: "Doe",
+    email: "johndoe3@example.com",
+    password: bcrypt.hashSync("password123", 10),
+    createdAt: Date.now(),
+    updatedAt: Date.now(),
+    userType: UserType.OPERATOR,
+    isVerified: true
+  }
 ];
 
 module.exports = { mockUsers };
